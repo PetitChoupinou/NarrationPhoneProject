5acc0830d27a57af459d76944d1550123ed7c9b1
Petit Choupinou <lebourhisalex11@gmail.com>
2026-05-12 11:42:27 +0200

    [ADD] MainLocation added
    It's where the player should start its journey

--- Assets/Scripts/Applications/Map/MapLocation.cs
+++ Assets/Scripts/Applications/Map/MapLocation.cs
@@ -4,19 +4,23 @@ public class MapLocation : MonoBehaviour
 {
     [SerializeField] MapLocationsInfos _infos;
     bool _isInfosActive;
+    [SerializeField] bool isMainLocation;
     [SerializeField] LocationData _data;
 
     public void SetupInstantiate(LocationData data)
     {
-        _infos.SetupInfos(data);
         _data = data;
+        Setup();
 
     }
 
     public void Setup()
     {
-        
         _infos.SetupInfos(_data);
+        if (isMainLocation)
+        {
+            PhoneManager.Instance.CurrentLocation = _data;
+        }
     }
     
 
