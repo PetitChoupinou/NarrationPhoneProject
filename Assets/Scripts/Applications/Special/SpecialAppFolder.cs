9e7c75d5d5748dc7a8184c32ed96faa2cd93b516
Emile Birraux <112955984+Birox1er@users.noreply.github.com>
2026-05-19 12:57:00 +0200

    [MOD] forgot smthing

--- Assets/Scripts/Applications/Special/SpecialAppFolder.cs
+++ Assets/Scripts/Applications/Special/SpecialAppFolder.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class SpecialAppFolder : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Setup(SpecialFolder folder)
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
