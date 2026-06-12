4e0e9db306bf5ef68236deb854f76ee42a6096c7
Petit Choupinou <lebourhisalex11@gmail.com>
2026-06-12 15:58:38 +0200

    [ADD] Reset all dialogues method

--- Assets/Scripts/General/SceneLoader.cs
+++ Assets/Scripts/General/SceneLoader.cs
@@ -9,8 +9,8 @@ public class SceneLoader : MonoBehaviour
 
     [SerializeField] private string _sceneToLoad = "TestTexts";
     [SerializeField] private CanvasGroup _splashScreen;
-    [SerializeField] private float _loadingTime=1.2f;
-    [SerializeField] public List<StoryAppSetup> _story=new List<StoryAppSetup>();
+    [SerializeField] private float _loadingTime = 1.2f;
+    [SerializeField] public List<StoryAppSetup> _story = new List<StoryAppSetup>();
     private StoryAppSetup _chosenStory;
     private float _timer = 0;
 
@@ -36,27 +36,38 @@ public class SceneLoader : MonoBehaviour
     }
     IEnumerator LoadGameSceneAsync()
     {
-        AsyncOperation operation= SceneManager.LoadSceneAsync(_sceneToLoad);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(_sceneToLoad);
         operation.allowSceneActivation = false;
-        while( _timer<_loadingTime)
+        while (_timer < _loadingTime)
         {
             _timer += Time.deltaTime;
-            _splashScreen.alpha = Mathf.Lerp(0.0f, 1.0f, _timer / (_loadingTime-.2f));
-                yield return null;
+            _splashScreen.alpha = Mathf.Lerp(0.0f, 1.0f, _timer / (_loadingTime - .2f));
+            yield return null;
         }
         _timer = 0;
         operation.allowSceneActivation = true;
-        while(!operation.isDone)//if the scene is not loaded yet we wait for it to be 
+        while (!operation.isDone)//if the scene is not loaded yet we wait for it to be 
         {
             yield return null;
         }
         while (_timer < _loadingTime) // and then we fade out the splash scrren
-        { 
+        {
             _timer += Time.deltaTime;
             _splashScreen.alpha = Mathf.Lerp(1.0f, 0.0f, _timer / _loadingTime);
             yield return null;
         }
         yield return null;
     }
+
+    private void ResetAllDialogues()
+    {
+        foreach (CharacterSheet character in _chosenStory.Characters)
+        {
+            foreach (DialogueData dialogue in character.Dialogues)
+            {
+                dialogue.ResetDialogue();
+            }
+        }
+    }
+    #endregion
 }
-#endregion
