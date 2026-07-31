b59697086f307d1702c64b049fe1e08ba2f2f7c3
Petit Choupinou <lebourhisalex11@gmail.com>
2026-07-31 15:40:16 +0200

    [FIX] Remove debug

--- Assets/Scripts/Applications/Message/MessageChoice.cs
+++ Assets/Scripts/Applications/Message/MessageChoice.cs
@@ -8,8 +8,6 @@ public class MessageChoice : MonoBehaviour
     {
         print(text.text);
         var currentConv = FindFirstObjectByType<MessageApp>();
-        Debug.Log("Current conv: " + currentConv);
-        Debug.Log("Discussion: " + currentConv.CurrentConv.GetComponent<Discussion>());
         currentConv.CurrentConv.GetComponent<Discussion>().Choose(text.text);
     }
     
