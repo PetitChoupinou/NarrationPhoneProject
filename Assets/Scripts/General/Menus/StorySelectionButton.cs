63a4ee3592e7a24cbf35c44dce1b396ca6ba10a9
Emile Birraux <112955984+Birox1er@users.noreply.github.com>
2026-07-31 10:39:20 +0200

    [MOD] Menu compréhensible

--- Assets/Scripts/General/Menus/StorySelectionButton.cs
+++ Assets/Scripts/General/Menus/StorySelectionButton.cs
@@ -1,14 +1,17 @@
+using TMPro;
 using UnityEngine;
 
 public class StorySelectionButton : MonoBehaviour
 {
     private SceneLoader _loader;
     private StoryAppSetup _setup;
+    [SerializeField] private TMP_Text _text;
 
     public void SetUp(StoryAppSetup setup)
     {
         _loader = FindFirstObjectByType<SceneLoader>();
         _setup = setup;
+        _text.text = setup.Name;
     }
     public void ChooseStory()
     {
